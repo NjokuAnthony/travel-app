21b11e2ddcdf61174287a93949b4f47a40050b2f
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-08-31 21:38:48 +0100

    finished the search components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,14 @@ const Search = () => {
         <div className='lg:col-span-2 flex flex-col justify-evenly'>
             <div>
                 <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
-                <P className='py-4'>Come experience the very pinnacle of Caribbean all-inclusive vacations for couples at BEACHES Resorts. Our Luxury beach resorts, set along the most gorgeous tropical settings and exquisite BEACHES in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and Curacao, feature unlimited gourment dining, unique bar serving premium liquors and wines, and very land and water sport, including complimentary green fees at our golf resorts and certified scuba diving at most resorts. if you are planning a wedding, BEACHES is the leader in Caribbean destinations weddings and honeymoon packages.</P>
+                <p className='py-4'>Come experience the very pinnacle of Caribbean all-inclusive vacations 
+                    for couples at BEACHES Resorts. Our Luxury beach resorts, set along the most gorgeous tropical 
+                    settings and exquisite BEACHES in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados 
+                    and Curacao, feature unlimited gourment dining, unique bar serving premium liquors and wines, 
+                    and very land and water sport, including complimentary green fees at our golf resorts and 
+                    certified scuba diving at most resorts. if you are planning a wedding, BEACHES is the leader 
+                    in Caribbean destinations weddings and honeymoon packages.
+                </p>
             </div>
             <div className='grid sm:grid-cols-2 gap-8 py-4'>
                 <div className='flex flex-col lg:flex-row items-center text-center'>
@@ -17,7 +24,7 @@ const Search = () => {
                     </button>
                     <div>
                         <h3 className='py-2'>LEADING SERVICE</h3>
-                        <P className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</P>
+                        <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
                     </div>
                 </div>
                 <div className='flex flex-col lg:flex-row items-center text-center'>
@@ -26,12 +33,41 @@ const Search = () => {
                     </button>
                     <div>
                         <h3 className='py-2'>AUTOMATED BOOKINGS</h3>
-                        <P className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</P>
+                        <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div></div>
+        <div>
+            <div className='border text-center'>
+                <p className='py-4'>GET AN ADDITIONAL 10% OFF</p>
+                <p className='py-4'>12 HOURS LEFT</p>
+                <p className='bg-gray-800 text-gray-200 py-2'>BOOK NOW AND SAVE</p>
+            </div>
+            <form className='w-full'>
+                <div className='flex flex-col my-2'>
+                <label>Destination</label>
+                    <select className='border rounded-md p-2'>
+                        <option>Grande Antigua</option>
+                        <option>Maldives</option>
+                        <option>Seychelles</option>
+                        <option>Zanzibar</option>
+                        <option>Osun Osogbo Sacred Groove</option>
+                        <option>Kruger Park</option>
+                        <option>Ogbunike Cave</option>
+                    </select>
+                </div>
+                <div className='flex flex-col my-4'>
+                    <label>Check-In</label>
+                    <input className='border rounded-md p-2' type="date" />
+                </div>
+                <div className='flex flex-col my-2'>
+                    <label>Check-Out</label>
+                    <input className='border rounded-md p-2' type="date" />
+                </div>
+                <button className='w-full my-4'>Rates & Availabilities</button>
+            </form>
+        </div>
     </div>
   )
 }
